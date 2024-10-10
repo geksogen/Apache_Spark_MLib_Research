2293547007039738d803140cd5375af401fa8078
Andrey.Torshin <dchorch@gmail.com>
2024-10-11 00:07:19 +0400

    load csv

--- Python/load_dt_pyspark.py
+++ Python/load_dt_pyspark.py
@@ -4,7 +4,7 @@ from operator import add
 
 from pyspark.sql import SparkSession
 import os
-
+import pandas as pd
 from pyspark import SparkFiles
 
 current_dir = os.path.dirname(__file__)
@@ -26,7 +26,14 @@ if __name__ == "__main__":
     # Load DATA from csv
     #df_sp_1 = spark.read.csv(header=True, inferSchema=True, path=petch)
     #df = spark.read.format("csv").option("header", "true").load("file:///home/sp-user/data_frame_1.csv")
-    df = spark.read.csv('https://raw.githubusercontent.com/geksogen/Apache_Spark_MLib_Research/refs/heads/master/Python/data_frame_1.csv')
-    df.show(10)
+    #df = spark.read.csv('https://raw.githubusercontent.com/geksogen/Apache_Spark_MLib_Research/refs/heads/master/Python/data_frame_1.csv')
+    #df.show(10)
+
+    url_github = 'https://raw.githubusercontent.com/AISCIENCES/course-master-big-data-with-pyspark-and-aws/main/Code/03-Spark%20DFs/StudentData.csv'
+
+    pd_df = pd.read_csv(url_github)
+    spark_df = spark.createDataFrame(pd_df)
+
+    spark_df.limit(5).show()
 
     spark.stop()
