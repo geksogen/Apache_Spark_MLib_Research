37fd95d379ef5a58c2d63a7804cd23a712e5ecb5
Andrei <test@test.mail>
2024-10-10 16:27:19 +0300

    load csv

--- Python/load_dt_pyspark.py
+++ Python/load_dt_pyspark.py
@@ -18,12 +18,12 @@ if __name__ == "__main__":
         .appName("Load two CSV")\
         .getOrCreate()
 
-    print(absolute_file_path)
+    #print(absolute_file_path)
     # Load DATA from csv
-    #df_sp_1 = spark.read.options(inferSchema='True',delimiter=',') \
-    #                        .csv("./data_frame_1.csv")
+    df_sp_1 = spark.read.options(inferSchema='True',delimiter=',') \
+                            .csv(absolute_file_path)
     #
     #df_sp_2 = spark.read.options(inferSchema='True',delimiter=',') \
     #                        .csv("./data_frame_2.csv")
-
+    print('OK!')
     spark.stop()
