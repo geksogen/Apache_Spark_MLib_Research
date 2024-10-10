487ef293a547b413d2ce2f487b8acdbed123c49b
Andrey.Torshin <dchorch@gmail.com>
2024-10-10 23:36:50 +0400

    load csv

--- Python/load_dt_pyspark.py
+++ Python/load_dt_pyspark.py
@@ -4,7 +4,7 @@ from operator import add
 
 from pyspark.sql import SparkSession
 import os
-
+from pyspark import SparkConf, SparkContext
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import current_timestamp
 from pyspark import SparkFiles
@@ -17,13 +17,19 @@ if __name__ == "__main__":
     """
         Usage: pi [partitions]
     """
-    spark = SparkSession\
-        .builder\
-        .appName("Load two CSV")\
-        .getOrCreate()
+    #spark = SparkSession\
+    #    .builder\
+    #    .appName("Load two CSV")\
+    #    .getOrCreate()
+
+    # Create a SparkConf object to configure the application
+    conf = SparkConf().setAppName("SparkContextExample").setMaster("local[*]")
+
+    # Create a SparkContext object
+    sc = SparkContext(conf=conf)
 
     data_file_https_url = "https://gist.githubusercontent.com/aakashjainiitg/dbb668c58839d68d7903f508bf55043c/raw/1feec07802b4f53aceac450fa1aee5a87d9276e0/cities_data_bank.csv"
-    spark.addFile(data_file_https_url)
+    sc.addFile(data_file_https_url)
     filePath = 'file://' + SparkFiles.get('cities_data_bank.csv')
     citiesDf = spark.read.csv(filePath, header=True, inferSchema=True)
 
