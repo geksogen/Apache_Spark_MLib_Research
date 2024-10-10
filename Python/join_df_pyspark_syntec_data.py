8bd426dfe4eda4e3b71596aba5280c224437f653
Andrei <test@test.mail>
2024-10-10 10:29:14 +0300

    python

--- Python/join_df_pyspark_syntec_data.py
+++ Python/join_df_pyspark_syntec_data.py
@@ -7,42 +7,6 @@ from random import sample
 import hashlib
 
 
-SIZE_DATAFRAME = 100
-list_index = sample(range(1, SIZE_DATAFRAME + 1), SIZE_DATAFRAME)
-list_value_df_2 = ['IT', 'Logistic', 'Legal']
-
-def rowValue(x, y):
-    return {'u_id': x,
-            'value_1': y
-            }
-
-def data_generator_1():
-    temp_list = []
-    for i in range(0, SIZE_DATAFRAME):
-        temp_requestType = random.choice(list_index)
-        value = hashlib.sha256((str(i)).encode('UTF-8')).hexdigest()
-
-        temp_list.append(rowValue(temp_requestType, value))
-
-    pd_dataframe_1 = pd.DataFrame(temp_list)
-    pd_dataframe_1.info()
-    pd_dataframe_1.to_csv('data_frame_1.csv', index=False)
-    return pd_dataframe_1
-
-def data_generator_2():
-    temp_list = []
-    for i in range(0, SIZE_DATAFRAME):
-        temp_requestType = random.choice(list_index)
-        value = random.choice(list_value_df_2)
-
-        temp_list.append(rowValue(temp_requestType, value))
-
-    pd_dataframe_2 = pd.DataFrame(temp_list)
-    pd_dataframe_2.info()
-    pd_dataframe_2.to_csv('data_frame_2.csv', index=False)
-    return pd_dataframe_2
-
-
 
 if __name__ == "__main__":
     """
@@ -53,8 +17,27 @@ if __name__ == "__main__":
         .appName("Join two dataframe")\
         .getOrCreate()
 
-    df_1 = data_generator_1()
-    df_2 = data_generator_2()
-    print('OK!')
+    SIZE_DATAFRAME = 10
+    list_index = sample(range(1, SIZE_DATAFRAME + 1), SIZE_DATAFRAME)
+    list_value_df_2 = ['IT', 'Logistic', 'Legal']
+
+    def rowValue(x, y):
+        return {'u_id': x,
+                'value_1': y
+                }
+
+    temp_list_1 = []
+    temp_list_2 = []
+
+    for i in range(0, SIZE_DATAFRAME):
+        temp_requestType = random.choice(list_index)
+        value_1 = hashlib.sha256((str(i)).encode('UTF-8')).hexdigest()
+        value_2 = random.choice(list_value_df_2)
+
+        temp_list_1.append(rowValue(temp_requestType, value_1))
+        temp_list_2.append(rowValue(temp_requestType, value_2))
+
+        pd_dataframe_1 = pd.DataFrame(temp_list_1)
+        pd_dataframe_2 = pd.DataFrame(temp_list_2)
 
     spark.stop()
