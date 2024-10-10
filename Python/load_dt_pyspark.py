a62f7394f815444385cf49eeede949dca6de140e
Andrei <test@test.mail>
2024-10-10 16:20:35 +0300

    load csv

--- Python/load_dt_pyspark.py
+++ Python/load_dt_pyspark.py
@@ -3,7 +3,11 @@ from random import random
 from operator import add
 
 from pyspark.sql import SparkSession
+import os
 
+current_dir = os.path.dirname(__file__)
+relative_path = "./data_frame_1.csv"
+absolute_file_path = os.path.join(current_dir, relative_path)
 
 if __name__ == "__main__":
     """
@@ -11,14 +15,15 @@ if __name__ == "__main__":
     """
     spark = SparkSession\
         .builder\
-        .appName("Join two dataframe")\
+        .appName("Load two CSV")\
         .getOrCreate()
 
+    print(absolute_file_path)
     # Load DATA from csv
-    df_sp_1 = spark.read.options(inferSchema='True',delimiter=',') \
-                            .csv("./data_frame_1.csv")
-
-    df_sp_2 = spark.read.options(inferSchema='True',delimiter=',') \
-                            .csv("./data_frame_2.csv")
+    #df_sp_1 = spark.read.options(inferSchema='True',delimiter=',') \
+    #                        .csv("./data_frame_1.csv")
+    #
+    #df_sp_2 = spark.read.options(inferSchema='True',delimiter=',') \
+    #                        .csv("./data_frame_2.csv")
 
     spark.stop()
