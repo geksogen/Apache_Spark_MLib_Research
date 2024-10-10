1646fa95c4c3a68c73b2c1bb763a9b0645b17b69
Andrey.Torshin <dchorch@gmail.com>
2024-10-10 22:46:25 +0400

    load csv

--- Python/load_dt_pyspark.py
+++ Python/load_dt_pyspark.py
@@ -23,6 +23,6 @@ if __name__ == "__main__":
     # Load DATA from csv
     #df_sp_1 = spark.read.csv(header=True, inferSchema=True, path=petch)
     #df = spark.read.format("csv").option("header", "true").load("file:///home/sp-user/data_frame_1.csv")
-    df = spark.read.csv('file:///home/sp-user/data_frame_1.csv')
+    df = spark.read.csv('file:///home/sp-user/*.csv')
     df.show()
     spark.stop()
