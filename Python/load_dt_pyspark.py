3fa25fa341e039fbf90f83b4f635ffc2a938af65
Andrey.Torshin <dchorch@gmail.com>
2024-10-10 23:23:06 +0400

    load csv

--- Python/load_dt_pyspark.py
+++ Python/load_dt_pyspark.py
@@ -5,6 +5,8 @@ from operator import add
 from pyspark.sql import SparkSession
 import os
 
+from pyspark import SparkFiles
+
 current_dir = os.path.dirname(__file__)
 relative_path = "data_frame_1.csv"
 absolute_file_path = os.path.join(current_dir, relative_path)
@@ -23,6 +25,8 @@ if __name__ == "__main__":
     # Load DATA from csv
     #df_sp_1 = spark.read.csv(header=True, inferSchema=True, path=petch)
     #df = spark.read.format("csv").option("header", "true").load("file:///home/sp-user/data_frame_1.csv")
-    df = spark.read.csv('file://" + spark.SparkFiles.get("data_frame_1.csv")')
-    df.show()
+    #df = spark.read.csv('file://" + spark.SparkFiles.get("data_frame_1.csv")')
+    #df.show()
+    spark.addFile("data_frame_1.csv")
+    print ("Absolute Path -> %s", spark.SparkFiles.get("data_frame_1.csv"))
     spark.stop()
