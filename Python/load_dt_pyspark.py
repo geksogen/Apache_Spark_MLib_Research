c8c5fa037fa2a03bc7c5648b0470a4569d1ac748
Andrey.Torshin <dchorch@gmail.com>
2024-10-10 23:29:45 +0400

    load csv

--- Python/load_dt_pyspark.py
+++ Python/load_dt_pyspark.py
@@ -5,12 +5,13 @@ from operator import add
 from pyspark.sql import SparkSession
 import os
 
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import current_timestamp
 from pyspark import SparkFiles
 
 current_dir = os.path.dirname(__file__)
 relative_path = "data_frame_1.csv"
 absolute_file_path = os.path.join(current_dir, relative_path)
-petch = "file://data_frame_1.csv"
 
 if __name__ == "__main__":
     """
@@ -21,11 +22,9 @@ if __name__ == "__main__":
         .appName("Load two CSV")\
         .getOrCreate()
 
+    data_file_https_url = "https://gist.githubusercontent.com/aakashjainiitg/dbb668c58839d68d7903f508bf55043c/raw/1feec07802b4f53aceac450fa1aee5a87d9276e0/cities_data_bank.csv"
+    spark.addFile(data_file_https_url)
+    filePath = 'file://' + SparkFiles.get('cities_data_bank.csv')
+    citiesDf = spark.read.csv(filePath, header=True, inferSchema=True)
 
-    # Load DATA from csv
-    #df_sp_1 = spark.read.csv(header=True, inferSchema=True, path=petch)
-    #df = spark.read.format("csv").option("header", "true").load("file:///home/sp-user/data_frame_1.csv")
-    #df = spark.read.csv('file://" + spark.SparkFiles.get("data_frame_1.csv")')
-    #df.show()
-    print ("Absolute Path -> %s", spark.SparkFiles.get("data_frame_1.csv"))
     spark.stop()
