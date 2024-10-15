23b4714945e94d6aa002c58da110a496a3dfdaf3
Andrei <test@test.mail>
2024-10-15 13:41:08 +0300

    read minio data

--- Python/load_dt_pyspark.py
+++ Python/load_dt_pyspark.py
@@ -6,11 +6,12 @@ from pyspark.sql import SparkSession
 import os
 import pandas as pd
 from pyspark import SparkFiles
+from s3fs import S3FileSystem
 
-current_dir = os.path.dirname(__file__)
-relative_path = "data_frame_1.csv"
-absolute_file_path = os.path.join(current_dir, relative_path)
-petch = "file://data_frame_1.csv"
+
+minio_endpoint = "http://89.169.153.72:9000/"
+access_key_id = 'u0IafcyUUrIaDfFDkWcA'
+secret_access_key = 'Iy4LGM83pPHOTNn4H6fCWw19kiXQNVkkKGLYLfID'
 
 if __name__ == "__main__":
     """
@@ -21,7 +22,19 @@ if __name__ == "__main__":
         .appName("Load two CSV")\
         .getOrCreate()
 
+    bucket_name = 'spark'
+    ticker_string_1 = 'data_frame_1.csv'
+    ticker_string_2 = 'data_frame_2.csv'
 
+
+    storage_options={
+       'key': access_key_id,
+       'secret': secret_access_key,
+       'endpoint_url': minio_endpoint,
+    }
+
+    url_github_1 = pd.read_csv(f's3://{bucket_name}/{ticker_string_1}', storage_options=storage_options)
+    url_github_2 = pd.read_csv(f's3://{bucket_name}/{ticker_string_2}', storage_options=storage_options)
 
     # Load DATA from csv
     #df_sp_1 = spark.read.csv(header=True, inferSchema=True, path=petch)
